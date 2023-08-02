dba9af572714c9b7e41e88d2bcf0e12e3b5890b8
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-02 18:32:31 -0500

    added q2

--- code-201/source.js
+++ code-201/source.js
@@ -1,7 +1,15 @@
 function questionOne(){
   let q1 = prompt("What is your name?");
   let a1 = q1;
-  alert("Nice name!");
-  document.write(a1);
+  alert("Nice name! Nice to meet you " + a1);
+  document.write("What is your name? " +"Answer: " +a1);
+
+}
+
+function questionTwo(){
+  let q2 = prompt("What do you like to do for fun?");
+  let a2 = q2;
+  alert("Interesting, so you like " + a2 + " I'd like to know more about that later..");
+  document.write("What do you like to do for fun? " +"Answer: " +a2);
 
 }
