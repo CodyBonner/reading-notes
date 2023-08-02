f45556b449a93b765d6f762e4eb4662779c8b244
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-02 12:24:06 +0000

    testing code2

--- code-201/source.js
+++ code-201/source.js
@@ -4,9 +4,3 @@ function questionOne(){
   alert("Nice name!");
   document.write(a1);
 }
-
-function questionTwo(){
-  let q2 = prompt("It's nice to meet you " + a1 + ", What do you do for a living?");
-  let a2 = q2;
-  alert(a2 + "sounds like an interesting job");
-}
