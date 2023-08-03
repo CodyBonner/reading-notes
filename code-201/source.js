a81a02b40b969a4a735de04f8ef3cc983a9f447c
Cody Bonner <bonner_cody94@yahoo.com>
2023-08-02 19:23:23 -0500

    testing new functions

--- code-201/source.js
+++ code-201/source.js
@@ -1,26 +1,19 @@
 
+let q1;
+let q2;
+let q3;
+let q4;
+let a1;
+let a2;
+let a3;
+let a4;
+
+
 
 function questionOne(){
-   let q1 = prompt("What is your name?");
-   let a1 = q1;
-   alert("Nice name! Nice to meet you " + a1);
+    q1 = prompt("What is your name?");
+    a1 = q1;
+   
    document.write("What is your name? " +"Answer: " +a1);
    
   }
-
-function questionTwo(){
-   let q2 = prompt("What do you like to do for fun?");
-   let a2 = q2;
-  alert("Interesting, so you like" + a2);
-  document.write("What do you like to do for fun? " +"Answer: " +a2);
-
-}
-
-
-function questionThree(){
-  let q3 = prompt("What job do you do?");
-  let a3 = q3;
-  alert("Ooo that's exciting having the job " + a3);
-  document.write("What job do you do? " +"Answer: " +a3);
-
-}
